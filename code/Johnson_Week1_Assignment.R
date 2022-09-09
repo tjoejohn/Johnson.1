b8d11d9fe32556e711ffb80d3d37bc393a9a16f9
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-09-09 16:14:02 -0400

    Wk1 graded

--- code/Johnson_Week1_Assignment.R
+++ code/Johnson_Week1_Assignment.R
@@ -5,7 +5,7 @@
 
 # Create 3 numeric vectors and 2 character vectors that are each 15 values in length with the following structures:
   # One character vector with all unique values
-  # One character vector with exactly 3 unique values
+  # One character vector with exactly 3 unique values 
   # One numeric vector with all unique values
   # One numeric vector with some repeated values (number of your choosing)
   # One numeric vector with some decimal values (of your choosing)
@@ -13,7 +13,7 @@
 
 
 a <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p")
-b <- c("q", "q", "q", "q", "q", "r", "q", "q", "s", "q", "q", "w", "q", "q", "q")
+b <- c("q", "q", "q", "q", "q", "r", "q", "q", "s", "q", "q", "w", "q", "q", "q") # four unique values here instead of three
 c <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 d <- c(15,16,17,18,19,20,18,21,22,23,18,24,25,18,26)
 e <- c(0.56,27,28,29,0.99,0.21,0.67,30,31,32,33,0.88,0.23,45,50)
@@ -33,21 +33,21 @@ colnames(df) <- c("lastinitial", "firstinitial", "passes", "touchdowns", "interc
 df
 
 #Chaacter vectors with unique values are already row names???, but supposed to use this code 
-row.names(df) <- df$playernumber
+row.names(df) <- df$playernumber #player number is not a column! That is why this did not work.
 df
 
 
 # Remove the character vector with unique values from the data frame.
 
 #To do this: 
-df$lastinitial <- NULL
+df$lastinitial <- NULL # you removed the column that should be row names without first making it row names.
 df
 
 #Except lastinitial would be name of whaever column you want to remove. 
 
 # or this which I could not get to move the correct column
-row.names(df1) <- df1$passes
-df <- df[,-1]
+row.names(df1) <- df1$passes #df1 is not an object. You've only created "df" so far.
+df <- df[,-1] #you've removed two columns now instead of one.
 df
 
 # Add 1 row with unique numeric values to the data frame
@@ -55,12 +55,12 @@ df
 #To do this use the following code 
 add.row <- data.frame(200,99,100.10,179)
 # next Bind rows:
-new_row
+new_row #new_row is not the object you just created.
 df <- rbind(df, new_row)
 tail(df)
 
 #IT will fail, so than do this to get it to work:
-names(new_row) <- names(df)
+names(new_row) <- names(df) #this does not work.
 df <- rbind(df, new_row)
 tail(df)
 
@@ -79,10 +79,10 @@ write.csv(df, file = "Week1assignment.csv")
 # Generate summary statistics of your data frame and copy them as text into your script under a new section heading.
 
 #To do this do:
-> summary(df)
+> summary(df) #need to get rid of these carrots.
 
 #Copyed and patsted summary statistics from my data frame: 
-
+#comment out these information.
 firstinitial          passes           touchdowns        interceptions     
 Length:16          Length:16          Length:16          Length:16         
 Class :character   Class :character   Class :character   Class :character  
@@ -91,3 +91,4 @@ Mode  :character   Mode  :character   Mode  :character   Mode  :character
 
 # Push your script and your .csv file to GitHub in a new "Week1" folder.
 #In excell, File > save as > CSV (Comma delimited)(*)
+#Why are you in Excel?
