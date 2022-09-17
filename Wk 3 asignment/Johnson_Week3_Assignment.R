c86c36b60b78d9f1af6ae450229eed9890289984
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2022-09-17 10:41:19 -0400

    Wk_3

---
+++ Wk 3 asignment/Johnson_Week3_Assignment.R
@@ -0,0 +1,38 @@
+# (1) Approximately how many hours ahead of Sunbury was the peak flow in Lewisburg during the 2011 flood? (2 pt)
+  
+#During the 2011 flood, the peak flow of the flood in Lewisburg was 9 hours and 45 minutes ahead to the peak flow time in sunbury
+
+# (2) Give one reason why information on the time between peak flow events up- and downstream could be valuable? (4 pts)
+
+#Information on the time between peak flow events up and downstream can be extremely valuable. Lets say the Susquehanna river is rising rapidly due to lots of rain and runoff. If the site or station upstream in Lewisburg revives data that the flow rate is exceeding flood limits, they warn the citizans who live downstream that a flood is likely to occur in Sunbury in however many hours they predict. This not only allows people who live close to or on the rive to quickly get valuables out of there house, but it also can save lives of the people that did not know the flood was coming.   
+
+
+# Package scavenger hunt! (12 pts each)
+
+## (3) Using Google and ONLY packages from GitHub or CRAN:
+    # Find a package that contains at least one function specifically designed to measure genetic drift.
+    # Copy-paste into your script - and run - an example from the reference manual for a function within this package related to a measure of genetic drift. 
+        # Depending on the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
+    # After running the function example, manipulate a parameter within the function to create a new result. 
+        # Common options might be allele frequency, population size, fitness level, etc. 
+        # Add the results of this manipulation to your script (if in the console) or upload the new plot.
+       
+          # By manipulating these parameters you can see how it impacts the results.
+          # This type of manipulation is one example of how theoretical ecology and modelling are used to predict patterns in nature.
+
+
+
+## (4) Using Google and ONLY packages from GitHub or CRAN:
+    # Find a package that will generate standard diversity metrics for community ecology, specifically Simpson's Diversity Index.
+    # Copy-paste into your script - and run - an example from the reference manual for a function to calculate Simpson's diversity. 
+        # Depending on the example usage of the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
+    # After running the function example, modify your script to generate another diversity metric that is NOT part of the example. 
+        # If there are two diversity metrics in the example script, neither of these will count as the modified script.
+        # Hint: If the function can "only" caluclate Simpson's diversity, the inverse of Simpson's diversity is another common metric. 
+        # Add the results of this manipulation to your script (if in the console) or upload the new plot.
+        
+          # Diversity metrics are frequently used in community ecology for reasons ranging from a quick comparison between sites to understanding community stability.
+          # Their calculation can be very tedious by hand - and very fast with a package designed for the operation.
+
+
+
--- code/Johnson_Week1_Assignment.R
+++ code/Johnson_Week1_Assignment.R
@@ -5,7 +5,7 @@
 
 # Create 3 numeric vectors and 2 character vectors that are each 15 values in length with the following structures:
   # One character vector with all unique values
-  # One character vector with exactly 3 unique values 
+  # One character vector with exactly 3 unique values
   # One numeric vector with all unique values
   # One numeric vector with some repeated values (number of your choosing)
   # One numeric vector with some decimal values (of your choosing)
@@ -13,7 +13,7 @@
 
 
 a <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p")
-b <- c("q", "q", "q", "q", "q", "r", "q", "q", "s", "q", "q", "w", "q", "q", "q") # four unique values here instead of three
+b <- c("q", "q", "q", "q", "q", "r", "q", "q", "s", "q", "q", "w", "q", "q", "q")
 c <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 d <- c(15,16,17,18,19,20,18,21,22,23,18,24,25,18,26)
 e <- c(0.56,27,28,29,0.99,0.21,0.67,30,31,32,33,0.88,0.23,45,50)
@@ -33,21 +33,21 @@ colnames(df) <- c("lastinitial", "firstinitial", "passes", "touchdowns", "interc
 df
 
 #Chaacter vectors with unique values are already row names???, but supposed to use this code 
-row.names(df) <- df$playernumber #player number is not a column! That is why this did not work.
+row.names(df) <- df$playernumber
 df
 
 
 # Remove the character vector with unique values from the data frame.
 
 #To do this: 
-df$lastinitial <- NULL # you removed the column that should be row names without first making it row names.
+df$lastinitial <- NULL
 df
 
 #Except lastinitial would be name of whaever column you want to remove. 
 
 # or this which I could not get to move the correct column
-row.names(df1) <- df1$passes #df1 is not an object. You've only created "df" so far.
-df <- df[,-1] #you've removed two columns now instead of one.
+row.names(df1) <- df1$passes
+df <- df[,-1]
 df
 
 # Add 1 row with unique numeric values to the data frame
@@ -55,14 +55,21 @@ df
 #To do this use the following code 
 add.row <- data.frame(200,99,100.10,179)
 # next Bind rows:
-new_row #new_row is not the object you just created.
+new_row
 df <- rbind(df, new_row)
 tail(df)
 
 #IT will fail, so than do this to get it to work:
-names(new_row) <- names(df) #this does not work.
+names(new_row) <- names(df)
 df <- rbind(df, new_row)
 tail(df)
+ 
+or
+# Match the names of the columns:
+colnames(add.row) <- colnames(df)    
+
+# Bind rows:
+df1 <- rbind(df, add.row)
 
 #obviosly "add.row" would be whatever the name of the data is you want to add.
 
@@ -79,10 +86,10 @@ write.csv(df, file = "Week1assignment.csv")
 # Generate summary statistics of your data frame and copy them as text into your script under a new section heading.
 
 #To do this do:
-> summary(df) #need to get rid of these carrots.
+> summary(df)
 
 #Copyed and patsted summary statistics from my data frame: 
-#comment out these information.
+
 firstinitial          passes           touchdowns        interceptions     
 Length:16          Length:16          Length:16          Length:16         
 Class :character   Class :character   Class :character   Class :character  
@@ -91,4 +98,3 @@ Mode  :character   Mode  :character   Mode  :character   Mode  :character
 
 # Push your script and your .csv file to GitHub in a new "Week1" folder.
 #In excell, File > save as > CSV (Comma delimited)(*)
-#Why are you in Excel?
