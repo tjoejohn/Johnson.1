13a53ce720db42c47ee486ed094b428141fd87bc
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2022-09-26 22:07:51 -0400

    posssible submit?

--- Wk 4 asignment/Johnson_Week4_GeneticsAssignment.R
+++ Wk 4 asignment/Johnson_Week4_GeneticsAssignment.R
@@ -1,12 +1,11 @@
 # Look at the plot and model results for our Dryad data in the tutorial. Part 1: Without knowing which points represent which groups, 
   # give one explanation for why these data might be difficult to draw spatial inferences about genes.(3 points)
 
-#Its hard to draw spatial inferences about genes from this plot because you can not really tell if there is a positive or negative trend on the plot, especially with the outlines on the very right side of the plot.
+#It's hard to draw spatial inferences about genes without knowing which points represent which groups because the net1 plot is a flowchart per say, and you can't determine the spacial inferences. 
 
   # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (3 points), and EXPLAIN WHY (4 points).
 
-#I belive that this plot has a negative trend. This is because the points from 300,000 to 750,000 on the X axis are mostly trending downward.
-
+#Despite the drawbacks, I can compare data points on a relative scale. This is because I can infer that Seq24 and Seq16 are 10 points/dots away from each other as well as Seq5 and Seq16. All the other Seq points/dots are less than 10 points apart from each other.
 
 # For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
   # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
@@ -22,20 +21,27 @@ data(ge_data)
 # Create two linear models for Yield Response: one related to the Environment and one to the Genotype. (2 points each)
   # 'Yield Response' in this dataset is a measure of phenotype expression.
   # Hint: Look at the help file for this dataset.
+?ge_data
+?lm
 
-data(ge_data) 
-YieldANOVA <-
-  add_anova(
-    .data = ge_data 
-    , .y = Yield 
-    , .rep = Rep 
-    , .gen = Gen 
-    , .env = Env 
-    ) 
-YieldANOVA
-data(ge_data)
+Kyle <- lm(ge_data$Yield ~ ge_data$Env)
+Busch <- lm(ge_data$Yield ~ ge_data$Gen)
 # Test the significance of both models and look at the model summary. (3 points each)
+anova(Kyle)
+summary(Kyle)
+
+anova(Busch)
+summary(Busch)
+
   # Which model is a better fit to explain the yield response, and WHY? (6 points)
   # Hint: Does one model seem more likely to be over-fitted?
 
+#I belive the data that yields Environment (the linear model I named "Kyle") is a better fit to explain the yield response. This is because the R squared value is larger (which means a stronger relationship) than linear model that yields genotype (the linear model named Busch). 
+
 # Which environment would be your very WORST choice for generating a strong yield response? (2 points)
+
+#The environment that would be the worst for generating a strong yield response would be the environment that yields genotype. This is because the R squared value is small, and would not generate a strong yield repsonse. 
+
+#Always rember to set working directory!!!!!!!!
+setwd("C:/GitHub/Johnson.1/Wk 4 asignment")
+getwd()
