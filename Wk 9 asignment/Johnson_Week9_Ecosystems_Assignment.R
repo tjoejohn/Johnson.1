55595b06630ff57a3b7bb62363831c2091531783
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2022-10-25 22:44:34 -0400

    update

--- Wk 9 asignment/Johnson_Week9_Ecosystems_Assignment.R
+++ Wk 9 asignment/Johnson_Week9_Ecosystems_Assignment.R
@@ -7,6 +7,40 @@ getwd()
 # (Q1 - 12 pts) Use the dataset from the tutorial to complete one redundancy analysis (RDA) with variance partitioning on a different community (NOT the nematodes).
   # Explain the ecological importance of your significant predictor variables, or the importance if none are significant for your community.
 
+#Must install/read in read_excell package fist!
+
+library(readxl)
+
+#Next read in excel file and untransfrom data from tibble format using as.data.frame
+
+vegitation.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Vegetation_transects")
+vegitation <- as.data.frame(vegitation.tibble)
+
+invert.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Invertebrate_community")
+invert <- as.data.frame(invert.tibble)
+
+head(vegitation)
+head(invert)
+
+#There is a different amount of samples per invertabrete_community compared to Vegitation_transects. Run the following code to take an average/mean and make them even!
+
+vegitation.names <- paste(vegitation$Parcel, vegitation$Landuse)
+vegitation$names <- vegitation.names
+
+head(invert)
+
+#Do the same for the invertebrate(invert) community! 
+
+invert.names <- paste(invert$Parcel, invert$Landuse)
+invert$names <- invert.names
+
+#Agregate means by names to columns 
+
+vegitation.means <- aggregate(x = vegitation, by = list(vegitation$names), FUN = "mean")
+
+invert.means <- aggregate(x = invert, by = list(invert$names), FUN = "mean")
+
+
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
 
