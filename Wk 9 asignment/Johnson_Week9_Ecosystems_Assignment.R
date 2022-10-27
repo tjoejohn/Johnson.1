cd084a5f0838d60c6ea8e4a1af74d76ed91f31aa
tjoejohn <johnsontj@susqu.edu>
2022-10-27 09:58:08 -0400

    Update Johnson_Week9_Ecosystems_Assignment.R

--- Wk 9 asignment/Johnson_Week9_Ecosystems_Assignment.R
+++ Wk 9 asignment/Johnson_Week9_Ecosystems_Assignment.R
@@ -13,20 +13,19 @@ library(readxl)
 
 
 #Next read in excel file and untransfrom data from tibble format using as.data.frame
-
-vegitation.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Vegetation_transects")
-vegitation <- as.data.frame(vegitation.tibble)
+abiotic.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors")
+abiotic <- as.data.frame(abiotic.tibble)
 
 invert.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Invertebrate_community")
 invert <- as.data.frame(invert.tibble)
 
-head(vegitation)
+head(abiotic)
 head(invert)
 
-#There is a different amount of samples per invertabrete_community compared to Vegitation_transects. Run the following code to take an average/mean and make them even!
+#There is a different amount of samples per invertabrete_community compared to abiotic factors. Run the following code to take an average/mean and make them even!
 
-vegitation.names <- paste(vegitation$Parcel, vegitation$Landuse)
-vegitation$names <- vegitation.names
+abiotic.names <- paste(abiotic$Parcel, abiotic$Land_Use)
+abiotic$names <- abiotic.names
 
 head(invert)
 
@@ -37,11 +36,23 @@ invert$names <- invert.names
 
 #Agregate means by names to columns 
 
-vegitation.means <- aggregate(x = vegitation, by = list(vegitation$names), FUN = "mean")
+abiotic.means <- aggregate(x = abiotic, by = list(abiotic$names), FUN = "mean")
 
 invert.means <- aggregate(x = invert, by = list(invert$names), FUN = "mean")
 
-#Remove NA columns 
+#Remove NA columns for abiotic 
+
+
+abiotic.means1 <- abiotic.means[,-16] # NA column
+abiotic.means2 <- abiotic.means1[,-1:-6] # Plot and NA columns
+abiotic.means2 <- sapply(abiotic.means2, as.numeric ) # Make sure everything is numeric.
+abiotic.means2 <- as.data.frame(abiotic.means2) # Make sure it's in the right format.
+
+
+
+#Do same thing again, except for invertebrate!
+head(invert.means)
+
 invert.means1 <- invert.means[,-73] 
 head(invert.means1)
 invert.means2 <- invert.means1[,-1:-3]
@@ -51,24 +62,14 @@ invert.means2 <- sapply(invert.means2, as.numeric )
 #Make sure everything is in the right format!
 invert.means2 <- as.data.frame(invert.means2) 
 
-#Do same thing again, except for vegetation!
-head(vegitation.means)
-
-vegitation.means1 <- vegitation.means[,-41] 
-head(vegitation.means1)
-vegitation.means2 <- vegitation.means1[,-1:-3] 
-head(vegitation.means2)
-# Make sure everything is numeric.
-vegitation.means2 <- sapply(vegitation.means2, as.numeric ) 
-#Make sure everything is in the right format!
-vegitation.means2 <- as.data.frame(vegitation.means2) 
 
 #Install/read in this package and compare against vegetation transects (abiotic) against invertebrate communities (biotic) 
 library(vegan)
 
-colnames(vegitation.means2)
-ord <- rda(invert.means2 ~ Urtica_dioica + Hedera_helix +	Corylus_avellana + Glechoma_hederacea	+ Aegopodium_podagraria	+ Angelica_sylvestris	+ Polygonatum_multiflorum	+ Paris_quadrifolia	+ Ranunculus_ficaria	+ Primula_elatior	+ Carex_sylvatica	+ Brachypodium_sylvaticum	+ Athyrium_filixfemina + Dryopteris_filixmas + Dryopteris_carthusiana + Deschampsia_cespitosa + Acer_campestre + Geum_urbanum	+ Rubus_fruticosus + Geranium_robertianum	+ Ilex_aquifolium	+ Circaea_lutetiana +	Filipendula_ulmaria	+ Festuca_gigantea +	Arum_maculatum + Anthriscus_sylvestris + Salix_sp	Lamium purpureum + Lamium_album	Lamium_galeobdolon + Heracleum_sphondylium + Quercus_robur + Fraxinus_excelsior	+ Acer_pseudoplatanus	+ Ribes_rubrum + Ranunculus_acris	+ Ribes_uva-crispa, vegitation.means2)
+fix.issue <- merge(abiotic.means2, invert.means2, by = "Group.1", all= FALSE)
 
+colnames(abiotic.means2)
+ord <- rda(invert.means2 ~ pH + totalN + Perc_ash + Kalium + Magnesium + Ca + Al + TotalP + OlsenP, abiotic.means2)
 
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
