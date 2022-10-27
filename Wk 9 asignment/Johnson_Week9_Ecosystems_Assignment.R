3dc5974b31fb90daa7f782e3d74fa2474b5af081
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2022-10-26 20:44:33 -0400

    Update Johnson_Week9_Ecosystems_Assignment.R

--- Wk 9 asignment/Johnson_Week9_Ecosystems_Assignment.R
+++ Wk 9 asignment/Johnson_Week9_Ecosystems_Assignment.R
@@ -10,6 +10,9 @@ getwd()
 #Must install/read in read_excell package fist!
 
 library(readxl)
+colnames(vegitation.means2)
+ord <- rda(invert.means2 ~ Urtica_dioica + Hedera_helix +	Corylus_avellana + Glechoma_hederacea	+ Aegopodium_podagraria	+ Angelica_sylvestris	+ Polygonatum_multiflorum	+ Paris_quadrifolia	+ Ranunculus_ficaria	+ Primula_elatior	+ Carex_sylvatica	+ Brachypodium_sylvaticum	+ Athyrium_filixfemina + Dryopteris_filixmas + Dryopteris_carthusiana + Deschampsia_cespitosa + Acer_campestre + Geum_urbanum	+ Rubus_fruticosus + Geranium_robertianum	+ Ilex_aquifolium	+ Circaea_lutetiana +	Filipendula_ulmaria	+ Festuca_gigantea +	Arum_maculatum + Anthriscus_sylvestris + Salix_sp	Lamium purpureum + Lamium_album	Lamium_galeobdolon + Heracleum_sphondylium + Quercus_robur + Fraxinus_excelsior	+ Acer_pseudoplatanus	+ Ribes_rubrum + Ranunculus_acris	+ Ribes_uva-crispa, vegitation.means2)
+
 
 #Next read in excel file and untransfrom data from tibble format using as.data.frame
 
@@ -51,6 +54,21 @@ invert.means2 <- sapply(invert.means2, as.numeric )
 invert.means2 <- as.data.frame(invert.means2) 
 
 #Do same thing again, except for vegetation!
+head(vegitation.means)
+
+vegitation.means1 <- vegitation.means[,-41] 
+head(vegitation.means1)
+vegitation.means2 <- vegitation.means1[,-1:-3] 
+head(vegitation.means2)
+# Make sure everything is numeric.
+vegitation.means2 <- sapply(vegitation.means2, as.numeric ) 
+#Make sure everything is in the right format!
+vegitation.means2 <- as.data.frame(vegitation.means2) 
+
+#Install/read in this package and compare against vegitation transects (abiotic) against invertebrate communities (biotic) 
+library(vegan)
+
+
 
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
