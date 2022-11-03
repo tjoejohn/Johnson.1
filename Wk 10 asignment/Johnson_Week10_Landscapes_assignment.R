25a6c46a0dcae1fa9331d08994e1a761e484d1ef
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2022-11-03 15:46:39 -0400

    Update Johnson_Week10_Landscapes_assignment.R

--- Wk 10 asignment/Johnson_Week10_Landscapes_assignment.R
+++ Wk 10 asignment/Johnson_Week10_Landscapes_assignment.R
@@ -20,9 +20,7 @@ Trichoptera.csv <- read.csv("Trichoptera.csv", header=T)
 HabitatbyPatch.csv <- read.csv("HabitatbyPatch.csv", header=T)
 Sprawlers.csv <- read.csv("Sprawlers.csv", header=T)
 
-#Part 2: What is your interpretation of the pattern for each group individually, and the two in comparison, based on their mobility? (5 points)
-
-#Format data metrics 
+#Format data by metrics 
 
 PatchLatLon.mat <- as.matrix(PatchLatLon.csv[,-1])
 Trichoptera.mat <- as.matrix(Trichoptera.csv)
@@ -36,6 +34,55 @@ nb1 <- droplinks(nb, 19, sym=TRUE) #these drop specific values from the network 
 nb2 <- droplinks(nb1, 22, sym=TRUE)
 nb3 <- droplinks(nb2, 25, sym=TRUE)
 nb4 <- droplinks(nb3, 30, sym=TRUE)
+
+#Map the empty network onto real lat/lon data, aka sampling locations.
+bin.mat <- aem.build.binary(nb4, PatchLatLon.mat, unit.angle = "degrees", rot.angle = 90, rm.same.y = TRUE, plot.connexions = TRUE)
+#How does this network compare to the real lat/lon of the points?
+plot(PatchLatLon.mat[,2]~PatchLatLon.mat[,3], xlim = rev(c(76.75,77)))
+
+#Now weight the relationships between these real locations on a prescribed network:
+aem.ev <- aem(aem.build.binary=bin.mat)
+#Remove the rows where links were dropped and focus on vector data frame
+aem.df <- aem.ev$vectors[c(-19,-22,-25,-30),]
+#this creates a LOT of variables. How do we choose which to use?
+aem.df
+
+
+#We will use forward selection in this case because of the sheer number of variables.
+Space.rda <- rda(Trichoptera.mat, as.data.frame(aem.df))
+Space.r2a <- RsquareAdj(Space.rda)$adj.r.squared
+
+aem.fwd <- forward.sel(Trichoptera.mat,aem.df, adjR2thresh=Space.r2a)
+
+#To identify which variables are important, you can identify them in order:
+aem.fwd$order
+#Those variables can then be selected from within the original AEM data frame.
+#Notice the last part of this new use of rda() - there is a third matrix for habitat data!
+#To determine how much variance is explained by spatial relationships relative to local habitat we need to compare two rda results.
+#One rda for space controlling for habitat, and one for habitat controlling for space.
+SpaceNoHab.rda <- rda(Trichoptera.mat, as.data.frame(aem.df[,aem.fwd$order]), HabitatbyPatch.mat)
+SpaceNoHab.rda 
+anova(SpaceNoHab.rda, perm.max = 10000)
+RsquareAdj(SpaceNoHab.rda)
+
+#And the habitat controlling for space:
+HabNoSpace.rda <- rda(Trichoptera.mat, HabitatbyPatch.mat, as.data.frame(aem.df[,aem.fwd$order]))
+HabNoSpace.rda 
+anova(HabNoSpace.rda, perm.max = 10000)
+RsquareAdj(HabNoSpace.rda)
+
+#Bewllow is from turorial to help you answer question 1!
+#Now look at the variance explained by each:
+#Unconstrained variance is the same in each, but the constrained and conditional values change!
+#SpaceNoHab - 46% constrained and 26% conditional
+#HabNoSpace - 4.9% constrained and 67% conditional
+#How is this possible? Some variance can only be explained by the synergistic relationships of habitat that varies predictably with space.
+
+#Part 2: What is your interpretation of the pattern for each group individually, and the two in comparison, based on their mobility? (5 points)
+
+#Format data metrics 
+
+
 
 #Part 3: For each of your chosen groups of bugs, perform variable selection for the habitat data rather than the AEM data. Which habitat variables are significant for each? (10 points)
   # Definitions for the habitat column names:
